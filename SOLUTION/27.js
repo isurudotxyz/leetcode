b3c9bfe7ea13305ba1ad3fc4324d1ca483f38881
Isuru Anton Gunaratne <86460712+isurudotxyz@users.noreply.github.com>
2023-07-08 16:08:56 +0200

    Create 27.js

---
+++ SOLUTION/27.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+        var j = 0;
+    for(var i=0;i<nums.length;i++)
+    {
+        if(nums[i]!=val)
+        {
+            nums[j++] = nums[i];
+        }
+    }
+    return j;
+}
